6bbf07df090680929af4376f73be1f578721b505
unicorn.chk@gmail.com <unicorn.chk@gmail.com>
2025-01-27 21:25:59 +0800

    add sync to b12902079

--- sync-csie.bat
+++ sync-csie.bat
@@ -4,3 +4,5 @@ zip -r htdocs.zip htdocs
 move htdocs _site
 scp htdocs.zip unicorn@ws1.csie.ntu.edu.tw:~/
 ssh unicorn@ws1.csie.ntu.edu.tw "echo A | unzip htdocs"
+scp htdocs.zip b12902079@ws1.csie.ntu.edu.tw:~/
+ssh b12902079@ws1.csie.ntu.edu.tw "echo A | unzip htdocs"
