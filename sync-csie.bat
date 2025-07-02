c3dbc4d068460027ac21a8c714476a2fcc8a66fa
unicorn.chk@gmail.com <unicorn.chk@gmail.com>
2025-07-02 17:06:05 +0800

    new post: AIS3 2025 pre-exam

--- sync-csie.bat
+++ sync-csie.bat
@@ -4,6 +4,7 @@ zip -r htdocs.zip htdocs
 move htdocs _site
 scp htdocs.zip unicorn@ws1.csie.ntu.edu.tw:~/
 ssh unicorn@ws1.csie.ntu.edu.tw "echo A | unzip htdocs"
+del htdocs.zip
 
 call bundle exec jekyll build --base /~b12902079
 move _site htdocs
@@ -11,3 +12,4 @@ zip -r htdocs.zip htdocs
 move htdocs _site
 scp htdocs.zip b12902079@ws1.csie.ntu.edu.tw:~/
 ssh b12902079@ws1.csie.ntu.edu.tw "echo A | unzip htdocs"
+del htdocs.zip
